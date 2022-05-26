5b198ebec749ac052796721084f6e0cb3a8c6044
gauravsaxena1997 <gauravsaxena.jaipur@gmail.com>
2022-05-26 18:06:07 +0530

    V2 path update transpile with v0.10.0

--- cloudtraffic_v2/cloud-function/traffic_light_manager_actions.go
+++ cloudtraffic_v2/cloud-function/traffic_light_manager_actions.go
@@ -7,61 +7,6 @@ import (
 	"github.com/gomodule/redigo/redis"
 )
 
-func (m *trafficLightStruct) initTrafficLight() {
-	m.mom.InitTrafficLight()
-}
-
-func (m *trafficLightStruct) destroyTrafficLight() {
-	m.mom.DestroyTrafficLight()
-}
-
-func (m *trafficLightStruct) enterRed() {
-	m.ChangeColor("red")
-	m.mom.EnterRed()
-}
-
-func (m *trafficLightStruct) enterGreen() {
-	m.ChangeColor("green")
-	m.mom.EnterGreen()
-}
-
-func (m *trafficLightStruct) enterYellow() {
-	m.ChangeColor("yellow")
-	m.mom.EnterYellow()
-}
-
-func (m *trafficLightStruct) enterFlashingRed() {
-	m.ChangeColor("red")
-	m.mom.EnterFlashingRed()
-}
-
-func (m *trafficLightStruct) startWorkingTimer() {
-	m.mom.StartWorkingTimer()
-}
-
-func (m *trafficLightStruct) stopWorkingTimer() {
-	m.mom.StopWorkingTimer()
-}
-
-func (m *trafficLightStruct) startFlashingTimer() {
-	m.mom.StartFlashingTimer()
-}
-func (m *trafficLightStruct) stopFlashingTimer() {
-	m.mom.StopFlashingTimer()
-}
-
-func (m *trafficLightStruct) changeFlashingAnimation() {
-	flashColor := ""
-	if m.flashColor == "red" {
-		flashColor = "default"
-	} else {
-		flashColor = "red"
-	}
-
-	m.ChangeColor(flashColor)
-	m.mom.ChangeFlashingAnimation()
-}
-
 func (m *trafficLightMomStruct) initTrafficLight() {
 	publishResponse("begin", "", "true")
 	time.Sleep(2 * time.Second)
